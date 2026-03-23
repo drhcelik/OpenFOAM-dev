2be2087579b2183756666b10e42e7f4c15dc7f51
Henry Weller <http://cfd.direct>
2026-03-23 21:37:25 +0000

    GeometricField: Generalised readIfPresent to support MUST_READ
    which allows multiphaseEuler::phaseModel to conditionally construct the phase
    volume fraction dependent on the referencePhase setting, i.e. requires the
    reading of all but the reference phase, without the need to clone a copy of the
    volume fraction field which was a work-around for a problem with the Clang
    compiler.

--- src/OpenFOAM/fields/GeometricFields/GeometricField/GeometricField.C
+++ src/OpenFOAM/fields/GeometricFields/GeometricField/GeometricField.C
@@ -126,17 +126,7 @@ bool Foam::GeometricField<Type, GeoMesh, PrimitiveField>::readIfPresent()
     (
         this->readOpt() == IOobject::MUST_READ
      || this->readOpt() == IOobject::MUST_READ_IF_MODIFIED
-    )
-    {
-        WarningInFunction
-            << "read option IOobject::MUST_READ or MUST_READ_IF_MODIFIED"
-            << " suggests that a read constructor for field " << this->name()
-            << " would be more appropriate." << endl;
-    }
-    else if
-    (
-        this->readOpt() == IOobject::READ_IF_PRESENT
-     && this->headerOk()
+     || (this->readOpt() == IOobject::READ_IF_PRESENT && this->headerOk())
     )
     {
         readFields();
